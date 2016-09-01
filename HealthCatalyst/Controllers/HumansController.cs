f0df66d4ec7e10ff9b4e95fb397ce1cac9f1490c
airburple <airburple@hotmail.com>
2016-09-01 14:12:21 -0600

    Added Humans controller and updated RouteConfig

---
+++ HealthCatalyst/Controllers/HumansController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthCatalyst.Controllers
+{
+    public class HumansController : Controller 
+    {
+        public ActionResult Index()
+
+        {
+
+            return new ContentResult()
+            {
+                Content = "You are home!"
+            };
+        }
+    }
+}
