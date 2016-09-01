0112277ef8994f2caa32be66ccfb458445394560
airburple <airburple@hotmail.com>
2016-09-01 13:58:51 -0600

    Added the Human class to the models folder

---
+++ HealthCatalyst/Models/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthCatalyst.Models
+{
+    public class Human
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Interests { get; set; }
+        public string Hair { get; set;}
+        public string FileName { get; set; }
+        
+    }
+}
